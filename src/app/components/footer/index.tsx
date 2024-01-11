34cb24d80b257f0617473d5300d330368274cf71
matin <97048003+matinmolkara@users.noreply.github.com>
2024-01-11 10:30:47 +0330

    footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,3 +1,105 @@
 export function Footer() {
-    return null;
+  return (
+    <div className="grid grid-cols-3 gap-4 ">
+      <div className="flex justify-center">
+        <div className="text-right">
+          <p className="mt-4 flex align-middle" dir="rtl">
+          <span className="inline-block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 stroke-cyan-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                />
+              </svg>
+            </span>
+            <span>
+                021 852 2365
+            </span>
+          </p>
+          <p className="mt-4 flex align-middle" dir="rtl">
+            <span className="inline-block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 stroke-cyan-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                />
+              </svg>
+            </span>
+            <span>
+                0911 345 2121
+            </span>
+          </p>
+          <p className="mt-4 flex align-middle" dir="rtl">
+            <span className="inline-block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 stroke-cyan-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+            </span>
+            <span>یکشنبه و چهارشنبه ۹ تا ۱۶</span>
+          </p>
+          <p className="mt-4 flex align-middle" dir="rtl">
+            <span className="inline-block">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 stroke-cyan-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                />
+              </svg>
+            </span>
+            <span>خیرآباد - میدان امام خمینی جنب نواب صفوی- پلاک ۳۱</span>
+          </p>
+        </div>
+      </div>
+      <div className=""></div>
+      <div className="flex justify-center">
+        <div className="text-right">
+          <h4>NAGHMEMALEKI</h4>
+          <p>لورم ایپسوم دولور سیت امیت</p>
+          <p className="mt-4">
+            <strong>لورم ایپسوم دولور سیت امیت</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
