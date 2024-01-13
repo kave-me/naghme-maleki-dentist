c09bf9f69f1394fc0a3f173264ee2a8027b745cf
matin <97048003+matinmolkara@users.noreply.github.com>
2024-01-13 21:17:35 +0330

    footer and staff

--- src/app/components/home/staff/index.tsx
+++ src/app/components/home/staff/index.tsx
@@ -1,9 +1,33 @@
 import { SectionHeader } from "@/app/components/SectionHeader";
-
+import Image from "next/image";
+import { staffData } from "@/app/data/people";
+import "./staff.css";
 export default function StaffSection() {
   return (
     <section className={"flex flex-col  items-center rtl pt-32"}>
       <SectionHeader name={"همکاران"} category={"با متخصصان ما آشنا شوید"} />
+      <div className={"flex gap-4 mt-16 flex-wrap"}>
+        {staffData.map((s) => (
+          <div
+            key={s.firstname}
+            className={
+              "size-44 flex flex-col items-center gap-2  bg-white  border-primary-light border rounded-3xl p-2 staff-holder-0"
+            }
+          >
+            <div className="staff-image-holder">
+              <Image width="100" height="100" src={s.profile_picture} alt="" />
+            </div>
+            <h3 className={"text-xl font-bold text-secondary "}>
+             { `${s.firstname}
+              
+              ${s.lastname}`}
+            </h3>
+            <p>{s.occupation}</p>
+          </div>
+        ))}
+      </div>
+      
     </section>
+    
   );
 }
